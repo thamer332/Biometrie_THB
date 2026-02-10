a70fb8e9e585471236c76ac82df2c7ad6cba44e9
thamer332 <thamer332@users.noreply.github.com>
2026-02-10 18:55:13 +0100

    Convert app.py to import web_app for Render compatibility

--- app.py
+++ app.py
@@ -1,37 +1,4 @@
+from web_app import app
 
-from PIL import Image
-import matplotlib.pyplot as plt
-
-chemin_image = 'lena_color.gif'
-image = Image.open(chemin_image)
-
-plt.subplot(1, 1, 1)
-plt.imshow(image)
-plt.title("Image Originale")
-plt.axis("off")
-plt.show()
-
-image.save('image_originale.png')
-print("Image sauvegardée sous 'image_originale.png'")
-
-
-nouvelle_taille = (200, 200) 
-image_redimensionnee = image.resize(nouvelle_taille)
-
-
-plt.figure(figsize=(10, 5))
-
-plt.subplot(1, 2, 1)
-plt.imshow(image)
-plt.title("Image Originale")
-plt.axis("off")
-
-plt.subplot(1, 2, 2)
-plt.imshow(image_redimensionnee)
-plt.title("Image Redimensionnée)")
-plt.axis("off")
-
-plt.show()
-
-image_redimensionnee.save('image_redimensionnee.png')
-print("Image redimensionnée sauvegardée sous 'image_redimensionnee.png'")
+if __name__ == '__main__':
+    app.run()
